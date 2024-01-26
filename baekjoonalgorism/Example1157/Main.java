dfc679ffff2b84accf3b0daf59d474774f3aa19b
saya61 <msaki061@gmail.com>
2024-01-26 16:26:52 +0900

    2024/1/26

---
+++ baekjoonalgorism/Example1157/Main.java
@@ -0,0 +1,16 @@
+package baekjoonalgorism.Example1157;
+
+import java.util.Scanner;
+
+/**
+ * 백준 단계 6
+ * 1157번 문제
+ * '이윤동' Code
+ * Start : 2024/01/26 PM 4:30
+ * End : 2024/01/26 PM 4:
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
