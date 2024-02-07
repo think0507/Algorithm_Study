997c8b6ef3976f989e47678b92bff7124e890338
saya61 <msaki061@gmail.com>
2024-02-07 13:55:13 +0900

    2/7 브렌치 수정 후 commit

--- src/saya61/Example1157/Main.java
+++ src/saya61/Example1157/Main.java
@@ -1,4 +1,4 @@
-package seya61.BOJ1157;
+package saya61.Example1157;
 
 import java.io.*;
 import java.util.HashMap;
---
+++ src/saya61/Example2839/Main.java
@@ -0,0 +1,36 @@
+package saya61.Example2839;
+
+import java.util.Scanner;
+/**
+ * 2839번 문제
+ * '이윤동' Code
+ * Start : 2024/02/06 PM 4:05
+ * End : 2024/02/06 PM 4:10
+ * 과거 C로 풀었던 문제.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sugar = scanner.nextInt();
+
+        System.out.println(solution(sugar));
+    }
+
+    public static int solution(int sugar) {
+        // 설탕이 5kg 박스로만 포장이 가능할 때
+        if (sugar % 5 == 0) {
+            return sugar / 5;
+        }
+        // 설탕이 5kg 박스를 최대한 쓴 후 남은 설탕을 3kg 박스로 포장이 가능할 때
+        else {
+            for (int i = sugar / 5; i >= 0; i--) {
+                int box = sugar - (5 * i);
+
+                if (box % 3 == 0) {
+                    return i + (box / 3);
+                }
+            }
+        }
+        return -1;
+    }
+}
